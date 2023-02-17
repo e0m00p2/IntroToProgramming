4cae3796112cf424f436fd7065e881ac5bcd34a3
e0m00p2 <124808698+e0m00p2@users.noreply.github.com>
2023-02-17 17:05:05 +0000

    Revamped most of code to use GUI

--- main.py
+++ main.py
@@ -1,80 +1,89 @@
 #Erik Miller Intro to Programming Week 8 Assignment
+import tkinter as tk
 
-#Create vehicle class
 class Vehicle:
-  #Create function that can be called by a vehicle to set make
-  def setVehicleMake(self, make):
-    self.str_VehicleMake = make
+    def setVehicleMake(self, make):
+        self.str_VehicleMake = make
     
-  #Create function that can be called by a vehicle to set model
-  def setVehicleModel(self, model):
-    self.str_VehicleModel = model
-  #Create function that can be called by a vehicle to return make
-  def getVehicleMake(self):
-    return self.str_VehicleMake
-  #Create function that can be called by a vehicle to return model
-  def getVehicleModel(self):
-    return self.str_VehicleModel
+    def setVehicleModel(self, model):
+        self.str_VehicleModel = model
+        
+    def getVehicleMake(self):
+        return self.str_VehicleMake
+        
+    def getVehicleModel(self):
+        return self.str_VehicleModel
 
-#Create a Car class that inherits from Vehicle.
 class Car(Vehicle):
-  #Create function that can be called by a car to set number of doors.
-  def setCarDoors(self, doors):
-    self.str_CarDoor = (doors)
-    
-  #Create function that can be called by a car to get number of doors.
-  def getCarDoors(self):
-    return self.str_CarDoor
+    def setCarDoors(self, doors):
+        self.str_CarDoor = doors
+        
+    def getCarDoors(self):
+        return self.str_CarDoor
 
-#Create Truck class that inherits from Vehicle
 class Truck(Vehicle):
-  #Create function that sets length of truck bed.
-  def setBedLength(self, bedLength):
-    self.str_BedLength = bedLength
-  #Create function that gets length of truck bed.
-  def getBedLength(self):
-    return self.str_BedLength
+    def setBedLength(self, bedLength):
+        self.str_BedLength = bedLength
+        
+    def getBedLength(self):
+        return self.str_BedLength
 
-#Send introduction message to user
-print("Welcome to E0m00p2's Virtual Garage")
-print("")
-#Set selection variable for loop so that program repeats until you select to quit via inputting 3.
-selection = 0
-while int(selection) < int(3):
-  selection = input("Enter 1 to make a car, 2 to make a truck, or 3 to quit: ")
-  print("")
-  #If user selects 1, create a new car and ask for user to input make, model, and car door number, using functions made in vehicle and car class. 
-  #Print new cars details.
-  if (selection == '1'):
+def create_car():
     new_car = Car()
-    strMake = input("Please enter the car make:")
+    strMake = make_entry.get()
     new_car.setVehicleMake(strMake)
-    strModel = input("Please enter the car model:")
+    strModel = model_entry.get()
     new_car.setVehicleModel(strModel)
-    strDoors = input("Please enter the number of doors:")
+    strDoors = doors_entry.get()
     new_car.setCarDoors(strDoors)
-    print("")
-    print(f"The number of doors: {new_car.getCarDoors()}")
-    print(f"The make is: {new_car.getVehicleMake()}")
-    print(f"The model is: {new_car.getVehicleModel()}")
-    print("You car has been added to the Garage.")
-  #If user selects 2, create a new truck and ask for user to input make, model, and truck bed length number, using functions made in vehicle and truck class. 
-  #Print new truck details.
-  elif (selection == '2'):
+    result_label.config(text=f"The number of doors: {new_car.getCarDoors()}\nThe make is: {new_car.getVehicleMake()}\nThe model is: {new_car.getVehicleModel()}\nYour car has been added to the Garage.")
+
+def create_truck():
     new_truck = Truck()
-    strMake = input("Please enter the truck make:")
+    strMake = make_entry.get()
     new_truck.setVehicleMake(strMake)
-    strModel = input("Please enter the truck model:")
+    strModel = model_entry.get()
     new_truck.setVehicleModel(strModel)
-    strDoors = input("Please enter the bed length:")
-    new_truck.setBedLength(strDoors)
-    print("")
-    print(f"The bed length is: {new_truck.getBedLength()}")
-    print(f"The make is: {new_truck.getVehicleMake()}")
-    print(f"The model is: {new_truck.getVehicleModel()}")
-    print("Your truck has been added to the Garage.")
-  #If any number other than 1 or 2, print thank you. If 3 or above, loop is exited and program ends.
-  else:
-    print("")
-    print("Thank you for using E0m00p2's Garage.")
-  print("")
+    strLength = length_entry.get()
+    new_truck.setBedLength(strLength)
+    result_label.config(text=f"The bed length is: {new_truck.getBedLength()}\nThe make is: {new_truck.getVehicleMake()}\nThe model is: {new_truck.getVehicleModel()}\nYour truck has been added to the Garage.")
+
+def quit_program():
+    root.destroy()
+
+root = tk.Tk()
+root.title("E0m00p2's Virtual Garage")
+
+# Create labels
+make_label = tk.Label(root, text="Make:")
+model_label = tk.Label(root, text="Model:")
+doors_label = tk.Label(root, text="Doors:")
+length_label = tk.Label(root, text="Length:")
+result_label = tk.Label(root, text="")
+
+# Create entry fields
+make_entry = tk.Entry(root)
+model_entry = tk.Entry(root)
+doors_entry = tk.Entry(root)
+length_entry = tk.Entry(root)
+
+# Create buttons
+car_button = tk.Button(root, text="Create Car", command=create_car)
+truck_button = tk.Button(root, text="Create Truck", command=create_truck)
+quit_button = tk.Button(root, text="Quit", command=quit_program)
+
+# Add labels, entry fields, and buttons to the window
+make_label.grid(row=0, column=0)
+make_entry.grid(row=0, column=1)
+model_label.grid(row=1, column=0)
+model_entry.grid(row=1, column=1)
+doors_label.grid(row=2, column=0)
+doors_entry.grid(row=2, column=1)
+length_label.grid(row=3, column=0)
+length_entry.grid(row=3, column=1)
+car_button.grid(row=4, column=0)
+truck_button.grid(row=4, column=1)
+result_label.grid(row=5, column=0, columnspan=2)
+quit_button.grid(row=6, column=0, columnspan=2)
+
+root.mainloop()
